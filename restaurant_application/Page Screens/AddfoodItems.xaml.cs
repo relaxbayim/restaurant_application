5e3e21fa31c6bfb516214727136c08ee0d86d641
AREL <AREL@B504-08>
2018-03-01 22:06:03 +0300

    Add project files.

---
+++ restaurant_application/Page Screens/AddfoodItems.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace restaurant_application.Page_Screens
+{
+    /// <summary>
+    /// Interaction logic for AddfoodItems.xaml
+    /// </summary>
+    public partial class AddfoodItems : Page
+    {
+        public AddfoodItems()
+        {
+            InitializeComponent();
+        }
+
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
